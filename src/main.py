e017cad1af0929caaeaa13f25a61885fc1d61757
tarafriedrich <capsaicinfix@gmail.com>
2024-12-30 13:59:15 -0800

    clean up error logging, fix some issues with CIGAR validation

--- src/main.py
+++ src/main.py
@@ -7,68 +7,130 @@ import sys
 sys.path.append("./")
 from transcript import Transcript
 import pandas as pd
-import argparse 
+import argparse
+import logging
 
 
 def main():
-    argparser = argparse.ArgumentParser(description="Find genomic coordinates of queries in transcripts")   
-    transcript_file = argparser.add_argument("--transcript_file", type=str, help="Path to the transcript file", default="data/transcripts.tsv")  
-    query_file = argparser.add_argument("--query_file", type=str, help="Path to the query file", default="data/queries.tsv")
-    output_file = argparser.add_argument("--output_file", type=str, help="Path to the output file", default="output/output.tsv")  
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.DEBUG)
+    argparser = argparse.ArgumentParser(
+        description="Find genomic coordinates of queries in transcripts"
+    )
+    transcript_file = argparser.add_argument(
+        "-t",
+        "--transcript_file",
+        type=str,
+        help="Path to the transcript file",
+        default="data/transcripts.tsv",
+    )
+    query_file = argparser.add_argument(
+        "-q",
+        "--query_file",
+        type=str,
+        help="Path to the query file",
+        default="data/queries.tsv",
+    )
+    output_file = argparser.add_argument(
+        "-o",
+        "--output_file",
+        type=str,
+        help="Path to the output file",
+        default="output/output.tsv",
+    )
+    debug = argparser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Print debug information",
+        default=False,
+    )
     args = argparser.parse_args()
     # Read the data from the file
-    transcripts = pd.read_csv(
-        args.transcript_file,
-        sep="\t",
-        names=["transcript_id", "chromosome", "genomic_start", "cigar"],
-    )
-    queries = pd.read_csv(args.query_file, sep="\t", names=["query_id", "tx_start"])
+    try:
+        transcripts = pd.read_csv(
+            args.transcript_file,
+            sep="\t",
+            names=["transcript_id", "chromosome", "genomic_start", "cigar"],
+            dtype={
+                "transcript_id": str,
+                "chromosome": str,
+                "genomic_start": int,
+                "cigar": str,
+            },
+            header=None,
+        )
+        if transcripts.shape[1] != 4:
+            raise ValueError(
+                "Transcript file must contain exactly four columns: transcript_id, chromosome, genomic_start, cigar"
+            )
+        if transcripts.isnull().values.any():
+            raise ValueError("Transcript file contains empty values")
+        if transcripts["genomic_start"].dtype != int:
+            raise ValueError("genomic_start must be an integer")
+    except Exception as e:
+        logger.error(f"Error reading transcript file {args.transcript_file}: {e}")
+        sys.exit(1)
+
+    try:
+        queries = pd.read_csv(
+            args.query_file,
+            sep="\t",
+            names=["query_id", "tx_start"],
+            header=None,
+            dtype={"query_id": str, "tx_start": int},
+        )
+        if queries.shape[1] != 2:
+            raise ValueError(
+                "Query file must contain exactly two columns: query_id and tx_start"
+            )
+        if queries.isnull().values.any():
+            raise ValueError("Query file contains empty values")
+        if queries["tx_start"].dtype != int:
+            raise ValueError("tx_start must be an integer")
+    except Exception as e:
+        logger.error(f"Error reading query file {args.query_file}: {e}")
+        sys.exit(1)
 
     # Store each transcript in a dictionary for faster lookup
     all_transcripts = {}
     for _, row in transcripts.iterrows():
-        transcript = Transcript(
-            row["transcript_id"], row["chromosome"], row["genomic_start"], row["cigar"]
-        )
-        print(f"Transcript : {transcript.transcript_id}")
-        print(f"CIGAR : {transcript.cigar}")
-        transcript.visualize_cigar()
-        transcript.precompute_tx_intervals()
-        transcript.precomputeIntervals2()
-        all_transcripts[transcript.transcript_id] = transcript
+        try:
+            transcript = Transcript(
+                row["transcript_id"],
+                row["chromosome"],
+                row["genomic_start"],
+                row["cigar"],
+            )
+            transcript.precomputeIntervals2()
+            all_transcripts[transcript.transcript_id] = transcript
+        except Exception as e:
+            logger.error(f"Error creating transcript {row['transcript_id']}: {e}")
+            sys.exit(1)
 
-    # Find the genomic coordinates of the queries
+    # Process queries
     output = open(args.output_file, "w")
     for _, query in queries.iterrows():
         if query["query_id"] not in all_transcripts:
-            print(f"Transcript {query['query_id']} not found")
+            logger.warning(f"Transcript {query['query_id']} not found")
             continue
         transcript = all_transcripts[query["query_id"]]
-        try:
-            genomic_start_precomputed = (
-                transcript.get_genomic_coordinates_from_precomputed_intervals(
-                    int(query["tx_start"])
-                )
-            )
 
-        except Exception as e:
-            print(f"Error: {e}")
-            continue
         try:
             genomic_start_precomputed2 = (
                 transcript.get_genomic_coordinates_from_precomputed_intervals2(
                     int(query["tx_start"])
                 )
             )
+            if genomic_start_precomputed2 == 0:
+                logger.error(
+                    f"Error converting transcript {transcript.transcript_id} to genomic coordinates"
+                )
         except Exception as e:
-            print(f"Error: {e}")
+            logger.error(
+                f"Error converting transcript {transcript.transcript_id} to genomic coordinates: {e}"
+            )
             continue
-        print(
-            f"A: {query['query_id']}\t{query['tx_start']}\t{transcript.chromosome}\t{genomic_start_precomputed}"
-        )
-        print(
-            f"B: {query['query_id']}\t{query['tx_start']}\t{transcript.chromosome}\t{genomic_start_precomputed2}"
-        )
         output.write(
             f"{query['query_id']}\t{query['tx_start']}\t{transcript.chromosome}\t{genomic_start_precomputed2}\n"
         )
--- src/transcript.py
+++ src/transcript.py
@@ -14,17 +14,21 @@ class Transcript:
         self.transcript_id = transcript_id
         self.chromosome = chromosome
         if not isinstance(genomic_start, int):
-            raise ValueError("genomic_start must be an integer")
+            raise ValueError(
+                "genomic_start {} must be an integer".format(genomic_start)
+            )
         if genomic_start < 0:
-            raise ValueError("genomic_start must be a non-negative integer")
+            raise ValueError(
+                "genomic_start {} must be a non-negative integer".format(genomic_start)
+            )
         self.genomic_start = genomic_start
         self.cigar = cigar
         self.cigar_tuples = self.parse_cigar()  # List of tuples (length, operation)
         self.strand = (
             "+"  # Assume that the transcript is always mapped from genomic 5’ to 3’.
         )
-        self.tx_intervals = []  # List of tuples (start, end)
-        self.gx_intervals = []  # List of tuples (start, end)
+        # self.tx_intervals = []  # List of tuples (start, end)
+        # self.gx_intervals = []  # List of tuples (start, end)
         self.exonIntervals = None
         self.tx_len = 0
 
@@ -35,21 +39,37 @@ class Transcript:
         length, operation = cigar_tuple
         cigar_operations = set(["M", "I", "D", "N", "S", "H", "P", "=", "X"])
         if operation not in cigar_operations:
-            raise ValueError(f"Invalid operation {operation} in CIGAR string")
+            raise ValueError(f"Invalid operation {operation} in CIGAR string: double CIGAR cigar {self.cigar}")
         if (
             length <= 0
         ):  # I think technically zero is allowed but I can't understand the logic behind it
-            raise ValueError(f"Invalid length {length} in CIGAR string")
+            raise ValueError(f"Invalid length {length} in CIGAR string: double check CIGAR {self.cigar}")
 
     def parse_cigar(self):
         """
         Parse the CIGAR string and return a list of tuples (length, operation).
         """
-        cigar_tuples = re.findall(r"([0-9]+)([MIDNSHP=X]{1})", self.cigar)
-        cigar_tuples = [(int(length), operation) for length, operation in cigar_tuples]
-        for cigar_tuple in cigar_tuples:
-            self.valid_cigar(cigar_tuple)
-        return cigar_tuples
+        cigar_tuples = re.findall(
+            r"([0-9]+)([A-Z|a-z|:/?#@!$&" "'" "()*+,;=%[]{1})", self.cigar
+        )
+        cigar_tuples_check = [
+            (int(length), operation) for length, operation in cigar_tuples
+        ]
+        # check if the cigar string is valid or raise an error
+        if cigar_tuples_check == []:
+            raise ValueError("Invalid CIGAR string {}".format(self.cigar))
+        for one_cigar_tuple in cigar_tuples_check:
+            self.valid_cigar(one_cigar_tuple)
+            new_cigar = one_cigar_tuple[0] + one_cigar_tuple[1]
+        # check if the new cigar string is the same as the original cigar string
+        # if regex is not finding the cigar tuple pair, it drops the cigar tuple pair
+        # this will cause the new_cigar to be different from the original cigar
+        #print (new_cigar)
+        if new_cigar != self.cigar:
+            raise ValueError(
+                f"Invalid CIGAR string {self.cigar}: double check CIGAR {new_cigar}"
+            )
+        return cigar_tuples_check
 
     def visualize_cigar(self):
         """
@@ -71,57 +91,59 @@ class Transcript:
                 genomic_string += "-" * int(length)
                 query_string += "I" * int(length)
             else:
-                pass  # I don't want to deal with  H, P
+                pass  # I don't want to deal with  H, P because they don't affect the genomic position
         print(f"Genomic: {genomic_string}")
         print(f"Query:   {query_string}")
         print(f"Count:   {count_string}")
 
-    def precompute_tx_intervals(self):
-        """
-        Precompute the intervals of the transcript.
-        """
-        tx_intervals = []
-        gx_intervals = []
-        tx_pos = 0
-        gx_pos = self.genomic_start
-        for length, operation in self.cigar_tuples:
-            if operation in ["M", "=", "X"]:
-                tx_intervals.append((tx_pos, tx_pos + length - 1))
-                gx_intervals.append((gx_pos, gx_pos + length - 1))
-                tx_pos += length
-                gx_pos += length
-            elif operation in ["D", "N"]:
-                tx_intervals.append(
-                    (-tx_pos, -tx_pos)
-                )  # negative values to indicate skipped regions
-                gx_intervals.append((gx_pos, gx_pos + length - 1))
-                gx_pos += length
-            elif operation in ["I", "S"]:
-                tx_intervals.append((tx_pos, tx_pos + length - 1))
-                gx_intervals.append(
-                    (-gx_pos, -gx_pos)
-                )  # negative values to indicate skipped regions
-                tx_pos += length
-            else:
-                pass  # I don't want to deal with H, P
+    # def precompute_tx_intervals(self):
+    #     """
+    #     Precompute the intervals of the transcript.
+    #     I don't use this because I don't consider it as effcient but it helps me check my logic.
+    #     """
+    #     tx_intervals = []
+    #     gx_intervals = []
+    #     tx_pos = 0
+    #     gx_pos = self.genomic_start
+    #     for length, operation in self.cigar_tuples:
+    #         if operation in ["M", "=", "X"]:
+    #             tx_intervals.append((tx_pos, tx_pos + length - 1))
+    #             gx_intervals.append((gx_pos, gx_pos + length - 1))
+    #             tx_pos += length
+    #             gx_pos += length
+    #         elif operation in ["D", "N"]:
+    #             tx_intervals.append(
+    #                 (-tx_pos, -tx_pos)
+    #             )  # negative values to indicate skipped regions
+    #             gx_intervals.append((gx_pos, gx_pos + length - 1))
+    #             gx_pos += length
+    #         elif operation in ["I", "S"]:
+    #             tx_intervals.append((tx_pos, tx_pos + length - 1))
+    #             gx_intervals.append(
+    #                 (-gx_pos, -gx_pos)
+    #             )  # negative values to indicate skipped regions
+    #             tx_pos += length
+    #         else:
+    #             pass  # I don't want to deal with H, P
 
-        print(f"Genomic intervals: {gx_intervals}")
-        print(f"Transcript intervals: {tx_intervals}")
-        # TODO: explore data structures to store intervals
-        self.tx_intervals = tx_intervals
-        self.gx_intervals = gx_intervals
+    #     print(f"Genomic intervals: {gx_intervals}")
+    #     print(f"Transcript intervals: {tx_intervals}")
+    #     # TODO: explore data structures to store intervals
+    #     self.tx_intervals = tx_intervals
+    #     self.gx_intervals = gx_intervals
 
-    def get_genomic_coordinates_from_precomputed_intervals(self, tx_start):
-        """
-        Given a transcript coordinate (tx_start), return the corresponding genomic coordinate using precomputed intervals.
-        """
-        for i, tx_interval in enumerate(self.tx_intervals):
-            tx_interval_start = tx_interval[0]
-            tx_interval_end = tx_interval[1]
-            if tx_start >= tx_interval_start and tx_start <= tx_interval_end:
-                tx_pos = tx_start - tx_interval_start
-                genomic_pos = self.gx_intervals[i][0] + tx_pos
-                return genomic_pos
+    # def get_genomic_coordinates_from_precomputed_intervals(self, tx_start):
+    #     """
+    #     Given a transcript coordinate (tx_start), return the corresponding genomic coordinate using precomputed intervals.
+    #     I don't use this because I don't consider it as effcient but it helps me check my logic.
+    #     """
+    #     for i, tx_interval in enumerate(self.tx_intervals):
+    #         tx_interval_start = tx_interval[0]
+    #         tx_interval_end = tx_interval[1]
+    #         if tx_start >= tx_interval_start and tx_start <= tx_interval_end:
+    #             tx_pos = tx_start - tx_interval_start
+    #             genomic_pos = self.gx_intervals[i][0] + tx_pos
+    #             return genomic_pos
 
     def precomputeIntervals2(self):
         """
@@ -140,7 +162,7 @@ class Transcript:
             elif operation in ["I", "S"]:
                 tx_pos += length
             else:
-                pass # I don't want to deal with H, P
+                pass # I don't want to deal with H, P because they don't affect the genomic position
         self.exonIntervals = exonIntervals
         self.tx_len = tx_pos
 
@@ -151,11 +173,7 @@ class Transcript:
         if self.exonIntervals is None or self.tx_len == 0:
             raise ValueError("Exon intervals have not been precomputed.")
         if tx_start < 0 or tx_start >= self.tx_len:
-            print(len(self.exonIntervals))
-            raise ValueError("tx_start is out of bounds.")
-
-        if tx_start < 0:
-            raise ValueError("tx_start must be a non-negative integer")
+            raise ValueError("tx_start {} is out of bounds.".format(tx_start))
         for interval in self.exonIntervals[tx_start]:
             return interval.data + (tx_start - interval.begin)
         return 0  # Return 0 if no interval is found
